82e3947d4dd7124ea04256b9185f293c7670e2ac
Jhon Majoor <jhonmajoor@gmail.com>
2023-09-17 15:23:17 +0200

    feat(): remove unnecessary files

--- src/Modalblanc.ts
+++ src/Modalblanc.ts
@@ -1,3 +1,19 @@
+type Animation = "fade-in-out";
+
+interface Settings {
+  animation: Animation;
+}
+
 export class Modalblanc {
-  constructor() {}
+  element: HTMLElement;
+  settings: Settings;
+
+  constructor(element: HTMLElement) {
+    this.element = element;
+    this.settings = { animation: "fade-in-out" };
+  }
+
+  open() {
+    console.info("open modal");
+  }
 }
